d5a783b29c918a117f904ba3e6983722af0896cb
arnaudmm <arnaudmm@gmail.com>
2013-12-01 12:29:11 +0100

    Update updateIds.php
    add strtolower for control

--- updateIds.php
+++ updateIds.php
@@ -2,7 +2,7 @@
 include_once('core.php');
 if ($handle = opendir('./uploads')) {
 	while (false !== ($entry = readdir($handle))) {
-		if(!is_dir('./uploads/'.$entry) && !in_array($entry,$ids) && $entry != 'index.html'){
+		if(!is_dir('./uploads/'.$entry) && !in_array(strtolower($entry),$ids) && $entry != 'index.html'){
 			$ids[uniqid()]=strtolower($entry);
 			store(ID_FILE,$ids);
 		}
