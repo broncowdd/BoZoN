85327c4ee1a7d56e352c16565a0d6b703eaaacea
arnaudmm <arnaudmm@gmail.com>
2013-12-01 12:39:30 +0100

    Update updateIds.php
    case issue

--- updateIds.php
+++ updateIds.php
@@ -4,6 +4,7 @@ if ($handle = opendir('./uploads')) {
 	while (false !== ($entry = readdir($handle))) {
 		if(!is_dir('./uploads/'.$entry) && !in_array(strtolower($entry),$ids) && $entry != 'index.html'){
 			$ids[uniqid()]=strtolower($entry);
+			rename('./uploads/'.$entry,'./uploads/'.strtolower($entry));
 			store(ID_FILE,$ids);
 		}
 	}
