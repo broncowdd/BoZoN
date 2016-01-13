e5194b25302e676c3d87757da12cb8763de88ca5
EauLand <philippe.lepetit@outlook.com>
2016-01-13 18:43:33 +0100

    Hearder
    Remove $_SESSION

--- themes/default/header.php
+++ themes/default/header.php
@@ -8,13 +8,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="application-name" content="BoZoN">
 <meta name="msapplication-tooltip" content="<?php e('Send, copy and share'); ?>">
-<meta name="msapplication-TileImage" content="themes/<?php echo $_SESSION['theme']; ?>/img/logo.png">
+<meta name="msapplication-TileImage" content="themes/default/img/logo.png">
 <meta name="msapplication-TileColor" content="#0088ec">
-<link rel="apple-touch-icon" href="themes/<?php echo $_SESSION['theme']; ?>/img/logo.png">
-<link rel="shortcut icon" type="image/png" href="themes/<?php echo $_SESSION['theme']; ?>/img/favicon.png">
-<link rel="stylesheet" type="text/css" href="themes/<?php echo $_SESSION['theme']; ?>/css/reset.css">
-<link rel="stylesheet" type="text/css" href="themes/<?php echo $_SESSION['theme']; ?>/css/style.css">
-<link rel="stylesheet" type="text/css" href="themes/<?php echo $_SESSION['theme']; ?>/css/mobile.css">
+<link rel="apple-touch-icon" href="themes/default/img/logo.png">
+<link rel="shortcut icon" type="image/png" href="themes/default/img/favicon.png">
+<link rel="stylesheet" type="text/css" href="themes/default/css/reset.css">
+<link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
+<link rel="stylesheet" type="text/css" href="themes/default/css/mobile.css">
 </head>
 
 <body>
@@ -22,7 +22,7 @@
     <p id="logo">&nbsp;</p>
     
     <div id="lang">
-      <a href="#"><img title="" src="themes/<?php echo $_SESSION['theme']; ?>/img/en.png" alt="" /></a>
-      <a href="#"><img title="" src="themes/<?php echo $_SESSION['theme']; ?>/img/fr.png" alt="" /></a>
-      <a href="#"><img title="" src="themes/<?php echo $_SESSION['theme']; ?>/img/es.png" alt="" /></a>
+      <a href="#"><img title="" src="themes/default/img/en.png" alt="" /></a>
+      <a href="#"><img title="" src="themes/default/img/fr.png" alt="" /></a>
+      <a href="#"><img title="" src="themes/default/img/es.png" alt="" /></a>
     </div>
