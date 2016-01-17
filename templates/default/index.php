de5ed6c5b51cb5e053020dbcaa35cb778ab0ae75
EauLand <philippe.lepetit@outlook.com>
2016-01-17 12:46:33 +0100

    Adaptation de l'index..php
    Adaptation de l'index.php pour la prochaine structure

--- templates/default/index.php
+++ templates/default/index.php
@@ -1,11 +1,11 @@
 <?php
   require __DIR__.'/header.php';
   
-  if(!empty($_SESSION['login'])){
+  if(isset($_SESSION['login']) && !empty($_SESSION['login'])){
 ?>
     <p id="status"><a title="<?php e('Logout'); ?>" href="?logout"><?php e('Logout'); ?></a></p>
 <?php
-  }else if(file_exists('data/account.php')){
+  }else if(file_exists('data/account/account.php')){
 ?>
     <p id="status"><a title="<?php e('Login'); ?>" href="login.php"><?php e('Login'); ?></a></p>
 <?php
