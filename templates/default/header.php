f8dd7c5b4e1b61de7166b4fabdeecf1e92fac876
EauLand <eauland@users.noreply.github.com>
2016-01-27 17:52:26 +0100

    Update header.php
    Modification de la couleur pour avoir le bon fond sur Firefox Mobile avec le nouveau logo.
    Modification du titre vu que le titre que j'avais mis étais modifié.

--- templates/default/header.php
+++ templates/default/header.php
@@ -7,9 +7,9 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="application-name" content="BoZoN">
-<meta name="msapplication-tooltip" content="<?php e('Send, copy and share'); ?>">
+<meta name="msapplication-tooltip" content="<?php e('Drag, drop, share.'); ?>">
 <meta name="msapplication-TileImage" content="<?php echo THEME_PATH;?>/img/favicon.png">
-<meta name="msapplication-TileColor" content="#0088ec">
+<meta name="msapplication-TileColor" content="#2c4aff">
 <link rel="apple-touch-icon" href="<?php echo THEME_PATH;?>/img/favicon.png">
 <link rel="shortcut icon" type="image/png" href="<?php echo THEME_PATH;?>/img/favicon.png">
 <link rel="stylesheet" type="text/css" href="<?php echo THEME_PATH;?>style.css">
