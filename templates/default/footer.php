6c356012120d6214fd2cdc464bc3d46db508b42a
Bronco <bronco@warriordudimanche.net>
2016-02-24 16:33:47 +0100

    Update footer.php

--- templates/default/footer.php
+++ templates/default/footer.php
@@ -3,7 +3,6 @@
 	  <p><a id="github" href="https://github.com/broncowdd/BoZoN" title="<?php e('Fork me on github'); ?>" target="_blank">&nbsp;</a></p>
 	  <p id="version">BoZoN <?php echo VERSION; ?></p>
 	</footer>
-	<script type="text/javascript" src="core/js/scrolltotop.js"></script>
 	<script>
 		// click on lightbox link
 		on('click','a[data-type]',function(){lb_show(this);});
